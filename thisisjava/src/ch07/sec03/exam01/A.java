aaac143e1ba5e1eed982588d4a2e58801ba8afe5
tatelulove4 <tatelulove4@naver.com>
2022-11-03 17:52:06 +0900

    221103

--- thisisjava/src/ch07/sec03/exam01/A.java
+++ thisisjava/src/ch07/sec03/exam01/A.java
@@ -4,7 +4,7 @@ package ch07.sec03.exam01;
 //자바에서 모든 클래스의 최상위 부모는 Object
 //일반 클래스는 extends Object가 생략되었다고 보면 됨.
 public class A extends Object{
-	public A() {
+	protected A() {
 		System.out.println("A 생성자");
 	}
 	public void method1() {
@@ -16,8 +16,20 @@ public class A extends Object{
 }
 
 class B extends A{
+	
 	public B() {
 		super();// 컴파일 시 추가 됨.
 		System.out.println("B 생성자");
 	}
+}
+
+class C{
+	public static void main(String[] args) {
+		B b = new B();
+		System.out.println(b.toString());
+		A a = new A();
+		System.out.println(a.toString());
+	}
+	
+	
 }
--- thisisjava/src/ch07/sec03/exam01/MainExample.java
+++ thisisjava/src/ch07/sec03/exam01/MainExample.java
@@ -4,6 +4,7 @@ public class MainExample extends Object{
 	public static void main(String[] args) {
 		B b = new B();
 		System.out.println(b.toString());
+		A a = new A();
 	}
 	
 }
